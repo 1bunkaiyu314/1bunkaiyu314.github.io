583c36e9cf6de02976b2ddec8e864fe4b2b23879
1bunkaiyu314 <1bunkaiyu314@gmail.com>
2026-04-04 13:33:46 +0900

    5組時間割表 ver. 0.0.1
    ・Service Worker追加。
    ・メタデータの微調整。
    ・ヘルプの微調整。

---
+++ class/timetableFor5/sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "timetable-cache-v2";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./assets/events.json",
+  "./assets/Hensoku.json",
+  "./assets/AorBorHen.json",
+  "./assets/terms.html",
+  "./assets/help.html",
+  "./assets/setting.html",
+  "./assets/source.html"
+];
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
