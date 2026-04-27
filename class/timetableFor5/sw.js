d27974b0e868c5019c56f3417aacabba8bfe2e60
1bunkaiyu314 <1bunkaiyu314@gmail.com>
2026-04-27 23:12:39 +0900

    ver. 2.0.0-δ

--- class/timetableFor5/sw.js
+++ class/timetableFor5/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "2.0.0-γ";
+const CACHE_NAME = "2.0.0-δ";
 
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
@@ -16,26 +16,26 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) =>
       cache.addAll([
-        "/assets/images/icons/icon-192.png",
-        "/assets/images/icons/icon-512.png",
-        "/assets/images/moon-dark.svg",
-        "/assets/images/sun-light.svg",
-        "/assets/json/timetable/timetable_class-1.json",
-        "/assets/json/timetable/timetable_class-2.json",
-        "/assets/json/timetable/timetable_class-3.json",
-        "/assets/json/timetable/timetable_class-4.json",
-        "/assets/json/timetable/timetable_class-5.json",
-        "/assets/json/timetable/timetable_class-6.json",
-        "/assets/json/timetable/timetable_class-7.json",
-        "/assets/json/timetable/timetable_class-8.json",
-        "/assets/json/timetable/timetable_class-9.json",
-        "/assets/json/events.json",
-        "/assets/setting.html",
-        "/assets/json/subjects_rooms_map.json",
-        "/assets/json/holidays.json",
-        "/index.html",
-        "/assets/js/app.js",
-        "/assets/css/style.css",
+        "./assets/images/icons/icon-192.png",
+        "./assets/images/icons/icon-512.png",
+        "./assets/images/moon-dark.svg",
+        "./assets/images/sun-light.svg",
+        "./assets/json/timetable/timetable_class-1.json",
+        "./assets/json/timetable/timetable_class-2.json",
+        "./assets/json/timetable/timetable_class-3.json",
+        "./assets/json/timetable/timetable_class-4.json",
+        "./assets/json/timetable/timetable_class-5.json",
+        "./assets/json/timetable/timetable_class-6.json",
+        "./assets/json/timetable/timetable_class-7.json",
+        "./assets/json/timetable/timetable_class-8.json",
+        "./assets/json/timetable/timetable_class-9.json",
+        "./assets/json/events.json",
+        "./assets/setting.html",
+        "./assets/json/subjects_rooms_map.json",
+        "./assets/json/holidays.json",
+        "./index.html",
+        "./assets/js/app.js",
+        "./assets/css/style.css",
       ])
     )
   );
